6dc2436964dd6a5adf9f94168eba170e1e0be7df
Oleksandr Kondratov <19samsung81k9@gmail.com>
2023-04-28 12:50:13 +0300

    commit1

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,16 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "@/components/layout/Layout";
 import AppContextProvider from "@/context/AppContext";
+import { SessionProvider } from "next-auth/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AppContextProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </AppContextProvider>
+    <SessionProvider session={pageProps.session}>
+      <AppContextProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </AppContextProvider>
+    </SessionProvider>
   );
 }
---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,7 @@
+import NextAuth from "next-auth/next";
+
+export const AuthOptions = {
+  providers: [],
+};
+
+export default NextAuth(AuthOptions);
